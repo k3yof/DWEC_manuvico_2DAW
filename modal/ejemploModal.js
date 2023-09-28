efcf033987bcd4ebc2477fc768c7d07734c2d5dc
k3yof <manuvicorevelles17@gmail.com>
2023-09-28 13:51:11 +0200

    a

--- modal/ejemploModal.js
+++ modal/ejemploModal.js
@@ -1,8 +1,13 @@
 // Ventana modal
 const modal = document.getElementById("ventanaModal");
+const modal1 = document.getElementById("ventanaModalI");
+const modal2= document.getElementById("ventanaModalR");
 
 // Botón que abre el modal
-const boton = document.getElementById("abrirModal");
+const boton = document.getElementById("español");
+const boton1 = document.getElementById("ingles");
+const boton2 = document.getElementById("ruso");
+
 
 // Hace referencia al elemento <span> que tiene la X que cierra la ventana
 const span = document.getElementsByClassName("cerrar")[0];
@@ -11,15 +16,37 @@ const span = document.getElementsByClassName("cerrar")[0];
 boton.addEventListener("click",function() {
     modal.style.display = "block";
 });
+boton1.addEventListener("click",function() {
+    modal1.style.display = "block";
+});
+boton2.addEventListener("click",function() {
+    modal2.style.display = "block";
+});
 
 // Si el usuario hace click en la x, la ventana se cierra
 span.addEventListener("click",function() {
     modal.style.display = "none";
+});
+span.addEventListener("click",function() {
+    modal1.style.display = "none";
+});
+span.addEventListener("click",function() {
+    modal2.style.display = "none";
 });
 
 // Si el usuario hace click fuera de la ventana, se cierra.
 window.addEventListener("click",function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+});
+window.addEventListener("click",function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+});
+window.addEventListener("click",function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 });
