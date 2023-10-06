93f59daa42b89a4dbf63259099706614a1ac3fa5
k3yof <manuvicorevelles17@gmail.com>
2023-10-06 08:50:17 +0200

    a

--- ejercicioPrompt/prompt.js
+++ ejercicioPrompt/prompt.js
@@ -2,3 +2,10 @@
 let nombre= prompt("Tu nombre porfa?", "");
 
 alert("Tu nombre es: " + nombre);
+
+const num1= Number(prompt("Num1?", ""));
+const num2= Number(prompt("Num2?", ""));
+
+const num3 = num1 + num2;
+
+alert("Tu suma es: " + num3);
---
+++ nombreJavaScript/prompt.js
@@ -0,0 +1,20 @@
+
+let nombre= prompt("Cual es el nombre inicial de javaScript?", "");
+
+
+if(nombre == 'ECMAScript')
+{
+    alert("Muy bien el nombre es : " + nombre);
+}else{
+    alert("Incorrecto el nombre es ECMAScript,no es : " + nombre);
+}
+
+let num = prompt('Dame un numero: ', 0);
+
+if (num > 0) {
+  alert( "Mayor que cero" );
+} else if (num < 0) {
+  alert( "Menor que cero");
+} else {
+  alert( "Igual que cero" );
+}
