08ff2748e40463908996a08b264437e8e53a703e
k3yof <manuvicorevelles17@gmail.com>
2023-10-06 10:14:34 +0200

    a

--- nombreJavaScript/prompt.js
+++ nombreJavaScript/prompt.js
@@ -18,3 +18,21 @@ if (num > 0) {
 } else {
   alert( "Igual que cero" );
 }
+
+let age = prompt('Dame un numero: ', 0);
+if (age >= 14 && age <= 90)
+{
+    alert ("Mayor que 14 y menor que 90");
+}
+
+let age1 = prompt('Dame un numero: ', 0);
+if (age1 <= 14 || age1 >= 90)
+{
+    alert ("No esta en el rango entre 14 y 90");
+}
+
+let age2 = prompt('Dame un numero: ', 0);
+if (!(age2 >= 14 && age2 <= 90))
+{
+    alert ("No esta en el rango");
+}
