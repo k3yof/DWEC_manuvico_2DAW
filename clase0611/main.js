807497c7cc37477d8f932ec7d484d11b13acadec
k3yof <manuvicorevelles17@gmail.com>
2023-11-06 12:38:49 +0100

    a

--- clase0611/main.js
+++ clase0611/main.js
@@ -30,5 +30,5 @@ function unique(arr) {
 }
 
 let strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
-
-console.log(unique(strings));
+unique(strings);
+console.log(strings);
